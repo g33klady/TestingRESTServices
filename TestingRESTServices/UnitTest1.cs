a2adf509a312f2d613e83e0c7976d544a9936bfe
hrobb <hilaryweaver-robb@quickenloans.com>
2016-09-05 23:28:13 -0400

    app.config and connecting to appsettings in unit test

--- TestingRESTServices/UnitTest1.cs
+++ TestingRESTServices/UnitTest1.cs
@@ -1,11 +1,21 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using System.Configuration;
 
 namespace TestingRESTServices
 {
 	[TestFixture]
 	public class UnitTest1
 	{
+		private static string _accessToken;
+		private static string _baseUri;
+
+		[OneTimeSetUp]
+		public void TestClassInitialize(TestContext context)
+		{
+			_accessToken = ConfigurationManager.AppSettings["accessToken"];
+			_baseUri = ConfigurationManager.AppSettings["baseUri"];
+		}
+
 		[Test]
 		public void TestMethod1()
 		{
