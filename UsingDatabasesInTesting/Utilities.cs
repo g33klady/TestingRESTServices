3b528fde26b35ede5c73272361a42c29c1b4b137
Hilary Weaver-Robb <hilaryweaver-robb@quickenloans.com>
2016-10-30 15:40:09 -0400

    updated tests and utilities to protect against sql injection - using parameters

--- UsingDatabasesInTesting/Utilities.cs
+++ UsingDatabasesInTesting/Utilities.cs
@@ -3,34 +3,44 @@ using System.Data.SqlClient;
 
 namespace UsingDatabasesInTesting
 {
-    public static class Utilities
-    {
-        public static DataTable ExecuteSqlQuery(string query, string connectionString)
-        {
-            using (var conn = new SqlConnection(connectionString))
-            {
-                using (var da = new SqlDataAdapter(query, conn))
-                {
-                    var dt = new DataTable();
-                    da.Fill(dt);
-                    return dt;
-                }
-            }
-        }
+	public static class Utilities
+	{
+		public static DataTable GetQuantityOfProduct(string productId, string connectionString)
+		{
+			string commandText = "SELECT Quantity FROM AdventureWorks2012.Production.ProductInventory WHERE ProductID = @ID;";
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				SqlCommand command = new SqlCommand(commandText, connection);
+				command.Parameters.Add("@ID", SqlDbType.NVarChar);
+				command.Parameters["@ID"].Value = productId;
+				using (var da = new SqlDataAdapter(command))
+				{
+					var dt = new DataTable();
+					da.Fill(dt);
+					return dt;
+				}
+			}
+		}
 
-        public static int ExecuteSqlCommand(string query, string connectionString)
-        {
-            int code = 0;
-            using (var conn = new SqlConnection(connectionString))
-            {
-                using (SqlCommand cmd = new SqlCommand(query, conn))
-                {
-                    conn.Open();
-                    code = cmd.ExecuteNonQuery();
-                    cmd.Dispose();
-                }
-            }
-            return code; //number of rows affected by the command if UPDATE, INSERT, DELETE
-        }
-    }
+		public static int UpdateQuantityOfProduct(string productId, int quantity, string connectionString)
+		{
+			int code = 0;
+			string commandText = "UPDATE AdventureWorks2012.Production.ProductInventory SET Quantity = @Quantity WHERE ProductID = @ID;";
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				using (SqlCommand command = new SqlCommand(commandText, connection))
+				{
+					command.Parameters.Add("@Quantity", SqlDbType.Int);
+					command.Parameters["@Quantity"].Value = quantity;
+					command.Parameters.Add("@ID", SqlDbType.NVarChar);
+					command.Parameters["@ID"].Value = productId;
+					connection.Open();
+					code = command.ExecuteNonQuery();
+					command.Dispose();
+				}
+			}
+			return code;
+		}
+
+	}
 }
